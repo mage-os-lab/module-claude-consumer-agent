5ad936ec4b0a8cbfad6ad72736d6f00392adbe48
Gregor Pollak <gregor@rocketweb.com>
2026-09-16 22:35:16 +0200

    Add the page config for Luma storefronts

---
+++ ViewModel/LumaConfig.php
@@ -0,0 +1,31 @@
+<?php
+declare(strict_types=1);
+
+namespace MageOS\ClaudeConsumerAgent\ViewModel;
+
+use Magento\Framework\View\Element\Block\ArgumentInterface;
+
+final class LumaConfig implements ArgumentInterface
+{
+    private const JSON_FLAGS = JSON_HEX_TAG | JSON_HEX_AMP | JSON_HEX_APOS | JSON_HEX_QUOT | JSON_UNESCAPED_SLASHES;
+
+    public function __construct(
+        private readonly \MageOS\ClaudeConsumerAgent\ViewModel\Assistant $assistant,
+        private readonly \MageOS\ClaudeConsumerAgent\ViewModel\CartSection $cartSection,
+        private readonly \Magento\Framework\Locale\FormatInterface $localeFormat
+    ) {
+    }
+
+    public function config(): array
+    {
+        return array_merge($this->assistant->snapshot(), [
+            'priceFormat' => $this->localeFormat->getPriceFormat(),
+            'cartUrl' => $this->cartSection->cartUrl(),
+        ]);
+    }
+
+    public function json(): string
+    {
+        return (string)json_encode($this->config(), self::JSON_FLAGS);
+    }
+}
